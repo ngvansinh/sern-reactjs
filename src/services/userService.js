9b42aa1a6dff748feabede3f8fecc89ee68ddc2a
Nguyen Van Sinh <nguyenvansinh1812@gmail.com>
2024-06-29 19:38:46 +0700

    sern-reactjs

---
+++ src/services/userService.js
@@ -0,0 +1,9 @@
+import axios from "../axios";
+
+const handleLoginApi = async (userEmail, userPassword) => {
+  return await axios.post("/api/login", {
+    email: userEmail,
+    password: userPassword,
+  });
+};
+export { handleLoginApi };
